d9eae8b2a36c2d4a21fef51b636fb9e9a7a11872
winsight <75029070+winsight@users.noreply.github.com>
2022-06-20 18:27:27 +0800

    2022-06-20

---
+++ fangte/data.js
@@ -0,0 +1,401 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "高速公路上",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8528636098241513,
+        "pitch": -0.2659683331777316,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7838899512585265,
+          "pitch": -0.296597056949647,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.8241398242300324,
+          "pitch": -0.6371212104558808,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "方特入口处岔路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.2090967629329512,
+        "pitch": 0.25959848240653116,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.905052544176609,
+          "pitch": -0.2614344647484579,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -1.2924108934245773,
+          "pitch": -0.003765201439277277,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.309281246458827,
+          "pitch": -1.2972249118684758,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "方特城堡左侧-远",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1099814353128465,
+        "pitch": -0.17185258399427283,
+        "fov": 1.7158664719103864
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.040482915532184,
+          "pitch": 0.21229492338722977,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -0.2206758177654855,
+          "pitch": -1.3326334808579592,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "方特城堡左侧-中",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.47370972718996285,
+        "pitch": 0.07384539888361275,
+        "fov": 1.2953967118379737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9436846005300374,
+          "pitch": -0.08982340638522857,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.5028284774684373,
+          "pitch": -1.4815394220890212,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "方特城堡左侧",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.9700792236343148,
+        "pitch": -0.15599837557517304,
+        "fov": 1.2953967118379737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19922683327933122,
+          "pitch": -0.00937651938159334,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.8486380099107613,
+          "pitch": -0.010561942348768838,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.5188715094335246,
+          "pitch": -1.4063386833353686,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "海螺湾",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.565902382473203,
+          "pitch": -1.1313698941927548,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "方特城堡右侧",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.623640702073864,
+        "pitch": -0.08559352213070603,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.688692369515282,
+          "pitch": -0.09199335973716316,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 0.45328917786330614,
+          "pitch": -1.4298271702110998,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "夜场舞台前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4857833443690573,
+        "pitch": 0.04464784268862765,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9609744868786514,
+          "pitch": -1.4553146424609142,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "随手拍",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.905872880398551,
+        "pitch": 0.2092419129050711,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8455564641009765,
+          "pitch": -1.186837448227072,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
